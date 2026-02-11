5e0b59a18fd777cbf007088da25d265ed1d4db1b
sebasyt300tylu <sebasyt300tylu@gmail.com>
2026-02-11 16:42:08 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let domain1 = ["sebastian", "sebas", "sebita", "cebolla"];
+let domain2 = ["elcrack", "elguapo", "caramonda", "papucho"];
+let domain3 = [".com", ".us", ".yahoo", ".net"];
+ 
+let resultado = '';
+
+for(let i=0; i < domain1.length; i++){
+  for(let j=0; j < domain2.length; j++){
+    for(let k=0; k < domain3.length; k++){
+      let finalGenerator = domain1[i] + domain2[j] + domain3[k];
+      resultado += `<li>${finalGenerator}</li>`;
+    }
+  }
+}
+document.getElementById('generator').innerHTML = resultado;
+
+
+
+
+
+
 };
