9478413b086f99fd85d3449f238fab389f38862c
sebasyt300tylu <sebasyt300tylu@gmail.com>
2026-02-16 01:53:59 +0000

    more specific names to variables

--- src/app.js
+++ src/app.js
@@ -6,16 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-let domain1 = ["sebastian", "sebas", "sebita", "cebolla"];
-let domain2 = ["elcrack", "elguapo", "caramonda", "papucho"];
-let domain3 = [".com", ".us", ".yahoo", ".net"];
+let names = ["sebastian", "sebas", "sebita", "cebolla"];
+let actions = ["elcrack", "elguapo", "caramonda", "papucho"];
+let domains = [".com", ".us", ".yahoo", ".net"];
  
 let resultado = '';
 
-for(let i=0; i < domain1.length; i++){
-  for(let j=0; j < domain2.length; j++){
-    for(let k=0; k < domain3.length; k++){
-      let finalGenerator = domain1[i] + domain2[j] + domain3[k];
+for(let i=0; i < names.length; i++){
+  for(let j=0; j < actions.length; j++){
+    for(let k=0; k < domains.length; k++){
+      let finalGenerator = names[i] + actions[j] + domains[k];
       resultado += `<li>${finalGenerator}</li>`;
     }
   }
